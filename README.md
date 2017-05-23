c05fa8e970f0bd95bc7a6aeeb35637306ed846f4
lianggzone <lianggzone@163.com>
2017-05-23 20:56:04 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 设计模式
+## 设计模式
+
+### 建造者模式
+- 文章 ：[暂无]
+- 地址 ： com.lianggzone.designpattern.builder
---
+++ src/main/java/com/lianggzone/designpattern/builder/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2013-2017 lianggzone all rights reserved.
+ * @license http://blog.720ui.com/about
+ */
+/**
+ * <p>建造者模式</p>
+ */
+package com.lianggzone.designpattern.builder;
